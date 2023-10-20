44ce32f662a5f3f878016036d9e7abc15b8d394e
nguyentienkhoi.it <83883786+nguyentienkhoiit@users.noreply.github.com>
2023-10-20 20:35:11 +0700

    Update Constants.java

--- src/main/java/com/capstone/backend/utils/Constants.java
+++ src/main/java/com/capstone/backend/utils/Constants.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class Constants {
     public static final String HOST = "http://localhost:8080";
-    public static final String HOST_SERVER = "https://github.com/nguyentienkhoiit/backend/tree/main/resource";
+    public static final String HOST_SERVER = "https://k4f4w9c2.stackpathcdn.com/wp-content/uploads/01_big_files_kim7/2023_best_ppt";
     public static final String API_VERSION = "/api/v1";
     public static final Long DEFAULT_PAGE_SIZE = 9L;
     public static final Long DEFAULT_PAGE_INDEX = 1L;
